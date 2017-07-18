55af54f9c88c14c629ce324e6dbe43fbf855e2ab
ashvaish\Ashish vaishnav <vaishnavashish5@gmail.com>
2017-07-18 22:39:55 +0530

    delete superuser

--- superuser/delete_user.php
+++
@@ -1,8 +0,0 @@
-<?php 
-								include("../connection.php");
-											if(isset($_GET['uid']))
-											{	@$check = $_GET['uid'];
-												$delete = mysql_query("DELETE FROM users WHERE uid = '".$check."' ");
-												header('Location:index.php');
-											}
-?>
