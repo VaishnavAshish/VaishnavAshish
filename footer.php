4848beeb621437e84f882b8360233a2195823d24
Akshay <akshaychauhan7737@gmail.com>
2017-07-19 21:14:59 +0530

    news bug fix

--- footer.php
+++ footer.php
@@ -33,19 +33,18 @@ if(isset($_POST['subscription'])){
 				<div  class="col-xs-12"  style="background-color:#031658; height:35px; width:100%; color:white;">
 					<h4  style="text-align:center;">Latest News<h4>
 				</div>
-				
-				<marquee direction="up">
+				<marquee direction="up" style="height:520px;">
 					<?php 
 						$homepage = file_get_contents('http://newsapi.org/v1/articles?source=the-times-of-india&sortBy=top&apiKey=aac20ad4883a41e0b8d8f4874e8168e1');
 						$news=json_decode($homepage,true);
 						foreach($news['articles'] as $value)
 						{ 
 					?>
-					<div class="col-xs-12" >
-					   <a href="<?php echo $value['url'];?>" target="_blank"
-					   <h4 class="text-primary"><?php echo $value['title'];?></h4></a>
-					   <p class="text-default"><?php echo $value['description'];?></p>
-					</div>
+							<div class="col-xs-12" >
+							<a href="<?php echo $value['url'];?>" target="_blank"
+							<h4 class="text-primary"><?php echo $value['title'];?></h4></a>
+							<p class="text-default"><?php echo $value['description'];?></p>
+							</div>
 						<?php } ?>
 				</marquee>
 			</div>
--- style/main.js
+++ style/main.js
@@ -75,7 +75,6 @@ function onSignIn(googleUser) {
   console.log('Image URL: ' + profile.getImageUrl());
   console.log('Email: ' + profile.getEmail()); // This is null if the 'email' scope is not present.
 }
-
 	$(document).on('submit','.login_form',function(){
 	    $.ajax({
 				 type:'post',
