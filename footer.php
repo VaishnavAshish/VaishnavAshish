d0f68389c93bfe1a8b578041c55b1236b2845e1a
Antriksh <2014pcecsantriksh@poornima.org>
2017-08-12 11:30:48 +0530

    no message

--- footer.php
+++ footer.php
@@ -29,7 +29,7 @@ if(isset($_POST['subscription'])){
 
 <!--NEWS-->
 <div class="col-sm-4 col-xs-12" style="max-width:100%; ">
-			<div class="col-xs-4" style="border:solid; background-color:white; height:560px; padding:0px; border:1; width:100%; margin-left:2%;"> 
+			<div class="col-xs-4" style="border:solid; background-color:white; height:560px; padding:0px; border:1; width:100%;"> 
 				<div  class="col-xs-12"  style="background-color:#031658; height:35px; width:100%; color:white;">
 					<h4  style="text-align:center;">Latest News<h4>
 				</div>
