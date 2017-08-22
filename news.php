09061a00902d7a121fd848d6507bcb3ed3db1b06
Antriksh <2014pcecsantriksh@poornima.org>
2017-08-22 21:26:53 +0530

    news and other changes

--- news.php
+++ news.php
@@ -1,23 +1,11 @@
+<link rel="stylesheet" href="style/css/news_scroll.css" type="text/css">
 <!--NEWS-->
 <div class="col-sm-4 col-xs-12" style="max-width:100%; ">
 			<div class="col-xs-4" style="border:solid; background-color:white; height:560px; padding:0px; border:1; width:100%;"> 
 				<div  class="col-xs-12"  style="background-color:#031658; height:35px; width:100%; color:white;">
 					<h4  style="text-align:center;">Latest News<h4>
 				</div>
-				<marquee direction="up" style="height:520px;">
-					<?php 
-						$homepage = file_get_contents('http://newsapi.org/v1/articles?source=the-times-of-india&sortBy=top&apiKey=aac20ad4883a41e0b8d8f4874e8168e1');
-						$news=json_decode($homepage,true);
-						foreach($news['articles'] as $value)
-						{ 
-					?>
-							<div class="col-xs-12" >
-							<a href="<?php echo $value['url'];?>" target="_blank"
-							<h4 class="text-primary"><?php echo $value['title'];?></h4></a>
-							<p class="text-default"><?php echo $value['description'];?></p>
-							</div>
-						<?php } ?>
-				</marquee>
+					<iframe name="NewsIFrame" src="news_scroll.php" frameborder="0" scrolling="no"></iframe>
 			</div>
 		</div>
 <!--/NEWS-->
--- register.php
+++ register.php
@@ -4,80 +4,80 @@ error_reporting(0);
 include('connection.php');
 session_start();
 
-// if(isset($_POST['userdata'])){
-					// $data=json_decode($_POST['userdata'], true);
-					// $name=$data['first_name']." ".$data['last_name'];
-					// $email=$data['email'];
-					// $fbid=$data['id'];
+if(isset($_POST['userdata'])){
+					$data=json_decode($_POST['userdata'], true);
+					$name=$data['first_name']." ".$data['last_name'];
+					$email=$data['email'];
+					$fbid=$data['id'];
 					
-					// $rsEmails = mysql_query("SELECT * FROM `users` WHERE email = '".$email."'");
-					// $numEmails = mysql_num_rows($rsEmails);
-					// $rsfb = mysql_query("SELECT * FROM `users` WHERE fbid = '".$fbid."'");
-					// $numfb = mysql_num_rows($rsfb);
+					$rsEmails = mysql_query("SELECT * FROM `users` WHERE email = '".$email."'");
+					$numEmails = mysql_num_rows($rsEmails);
+					$rsfb = mysql_query("SELECT * FROM `users` WHERE fbid = '".$fbid."'");
+					$numfb = mysql_num_rows($rsfb);
 					
-					// if($numfb > 0){
-								// $_SESSION['username']=$name;
-								// $_SESSION['email']=$email;
-								// $arr=['username'=>$name,'email'=>$email,'msg'=>'Logged in Successfully'];
-								// echo json_encode($arr);
-					// }
-					// else if($numEmails > 0 AND $numfb == 0) {
-						// @$query=mysql_query("UPDATE into `users` (fbid) values('$fbid')");
-						// $_SESSION['username']=$name;
-						// $_SESSION['email']=$email;
-						// $arr=['username'=>$name,'email'=>$email,'msg'=>'Logged in Successfully'];
-						// echo json_encode($arr);
-					// }
-					// else{
-							// @$query=mysql_query("insert into `users`(name,email,fbid) values('$name','$email','$fbid')");
-							// if($query)
-							// {
-								// $_SESSION['username']=$name;
-								// $_SESSION['email']=$email;
-								// $arr=['username'=>$name,'email'=>$email,'msg'=>'Registered Successfully'];
-								// echo json_encode($arr);
-							// }
-					// }
-				// }			
-// else if(isset($_POST['GMAIL'])){
-	// gmail Login
-	// $data=json_decode($_POST['GMAIL'], true);
-	// @$email=$data["U3"];
-	// @$name=$data["ig"];
-	// @$profileImage=$data["Paa"];
-	// @$gmailID=$data["Eea"];
-	// if(mysql_num_rows(mysql_query("SELECT * FROM `users` where email='".$email."'"))>0 && $email!=''){
-		// if(mysql_query("UPDATE `users` set name='".$name."',
-			// gmailid='".$gmailID."',
-			// profileImage='".$profileImage."'
-			// WHERE email='".$email."'")){
+					if($numfb > 0){
+								$_SESSION['username']=$name;
+								$_SESSION['email']=$email;
+								$arr=['username'=>$name,'email'=>$email,'msg'=>'Logged in Successfully'];
+								echo json_encode($arr);
+					}
+					else if($numEmails > 0 AND $numfb == 0) {
+						@$query=mysql_query("UPDATE into `users` (fbid) values('$fbid')");
+						$_SESSION['username']=$name;
+						$_SESSION['email']=$email;
+						$arr=['username'=>$name,'email'=>$email,'msg'=>'Logged in Successfully'];
+						echo json_encode($arr);
+					}
+					else{
+							@$query=mysql_query("insert into `users`(name,email,fbid) values('$name','$email','$fbid')");
+							if($query)
+							{
+								$_SESSION['username']=$name;
+								$_SESSION['email']=$email;
+								$arr=['username'=>$name,'email'=>$email,'msg'=>'Registered Successfully'];
+								echo json_encode($arr);
+							}
+					}
+				}			
+else if(isset($_POST['GMAIL'])){
+//	gmail Login
+	$data=json_decode($_POST['GMAIL'], true);
+	@$email=$data["U3"];
+	@$name=$data["ig"];
+	@$profileImage=$data["Paa"];
+	@$gmailID=$data["Eea"];
+	if(mysql_num_rows(mysql_query("SELECT * FROM `users` where email='".$email."'"))>0 && $email!=''){
+		if(mysql_query("UPDATE `users` set name='".$name."',
+			gmailid='".$gmailID."',
+			profileImage='".$profileImage."'
+			WHERE email='".$email."'")){
 				// UPDATE SUCCESS
-					// $_SESSION['username']=$name;
-					// $_SESSION['email']=$email;
-					// $arr=['username'=>$name,'email'=>$email,'msg'=>'Registered Successfully'];
-				// echo json_encode($arr);
-			// }else {
-				// echo json_encode(['username'=>null,'email'=>null,'msg'=>'Gmail Fail']);
-			// }
-	// }else {
+					$_SESSION['username']=$name;
+					$_SESSION['email']=$email;
+					$arr=['username'=>$name,'email'=>$email,'msg'=>'Registered Successfully'];
+				echo json_encode($arr);
+			}else {
+				echo json_encode(['username'=>null,'email'=>null,'msg'=>'Gmail Fail']);
+			}
+	}else {
 		// no entry found for the email
-			// if(mysql_query("INSERT INTO `users` set 
-				// name='".$name."',
-				// email='".$email."',
-				// gmailid='".$gmailID."',
-				// profileImage='".$profileImage."'")){
-					// INsert Success
-						// $_SESSION['username']=$name;
-						// $_SESSION['email']=$email;
-						// $arr=['username'=>$name,'email'=>$email,'msg'=>'Registered Successfully'];
-					// echo json_encode($arr);
-			// }else {
-				// echo json_encode(['username'=>null,'email'=>null,'msg'=>'Gmail Fail']);
-			// }
-	// }
-// }
-// else
-	// {
+			if(mysql_query("INSERT INTO `users` set 
+				name='".$name."',
+				email='".$email."',
+				gmailid='".$gmailID."',
+				profileImage='".$profileImage."'")){
+					//INsert Success
+						$_SESSION['username']=$name;
+						$_SESSION['email']=$email;
+						$arr=['username'=>$name,'email'=>$email,'msg'=>'Registered Successfully'];
+					echo json_encode($arr);
+			}else {
+				echo json_encode(['username'=>null,'email'=>null,'msg'=>'Gmail Fail']);
+			}
+	}
+}
+else
+	{
 
 //normal Register
      if(isset($_POST['register']))
@@ -125,5 +125,5 @@ session_start();
 				header("location:index.php");
 			}
 		 }
-			 
+	}		 
 ?>
