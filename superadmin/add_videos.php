6638550426e8accae16299262d2d083195ca1928
ashvaish\Ashish vaishnav <vaishnavashish5@gmail.com>
2017-09-22 10:03:03 +0530

    Change Password

--- superadmin/add_videos.php
+++ superadmin/add_videos.php
@@ -84,8 +84,9 @@ require('header.php');
                       <div class="form-group">
                         <div class="col-md-6 col-sm-6 col-xs-12 col-md-offset-3">
                           
-						  <button class="btn btn-primary" type="reset">Reset</button>
-                          <button type="submit" name="submit"class="btn btn-success">Submit</button>
+						  
+                          <button type="submit" name="submit" class="btn btn-success">Submit</button>
+													<button class="btn btn-primary" type="reset">Reset</button>
                         </div>
                       </div>
 
